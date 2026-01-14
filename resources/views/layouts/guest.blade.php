140eb81c597f622914d11d1a241665865340945b
Stanley Kaytovich <stanley@logicalogic.com>
2026-01-14 07:51:59 -0500

    Update login page styling for better visual consistency
    - Changed page background from light gray to dark gray (bg-gray-800) to match navigation sidebar
    - Changed form container from white to light gray (bg-gray-200) for better contrast against dark background

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -26,7 +26,7 @@
 </head>
 
 <body class="font-sans text-gray-900 antialiased">
-    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
+    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-800 dark:bg-gray-800">
         <div>
             <a href="/">
                 @if($logo)
